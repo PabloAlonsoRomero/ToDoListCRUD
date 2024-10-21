ee25ac59d7f61c243e2e4893c362d21947b3dd26
Pablo Alonso <pabloealonsor@gmail.com>
2024-10-21 17:36:42 -0600

    Feat: endpoint para crear una nueva tarea

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,23 @@ app.get("/todos", async (req: Request, res: Response) => {
     }
 })
 
+// Obtener una tarea por su ID
+app.get("/todos/:id", async (req: Request, res: Response) => {
+    try {
+        const {id} = req.params;
+        if (isNaN(Number(id))) {
+            throw { status: 400, message: "El ID debe ser un número valido"}
+        }
+        const tarea = await Tarea.findByPk(id);
+        if (!tarea) {
+            throw { status: 404, message: "Tarea no encontrada"}
+        }
+        res.json(tarea);
+    } catch (error) {
+        res.status(500).json({ message: "Error al obtener la tarea", error});
+    }
+})
+
 app.listen(3000, () => {
     console.log("Aplicacion ejecutandose en el puerto 3000")
 })

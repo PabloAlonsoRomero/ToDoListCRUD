32263b08c37d1ff4514eb4422e825fdf8df97c5f
Pablo Alonso <pabloealonsor@gmail.com>
2024-10-21 17:32:27 -0600

    Feat: endpoint obtener todas las tareas

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,19 @@ const app = express();
 app.use(express.json());
 dbConnection();
 
+// Obtener todas las tareas
+app.get("/todos", async (req: Request, res: Response) => {
+    try {
+        const tareas = await Tarea.findAll();
+        if (!tareas) {
+            throw { status: 404, message: "No se encontraron tareas"}
+        }
+        res.json(tareas);
+    } catch (error) {
+        res.status(500).json({ message: "Error al obtener las tareas"})
+    }
+})
+
 app.listen(3000, () => {
     console.log("Aplicacion ejecutandose en el puerto 3000")
 })
